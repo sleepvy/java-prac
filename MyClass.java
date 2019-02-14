ca1bae2d9cf42d27f5eaf0db136890883fadc2ea
sleepvy <vyle7651@colorado.edu>
2019-02-14 14:44:44 -0700

    first commit

---
+++ MyClass.java
@@ -0,0 +1,7 @@
+// Every line of code needs to be inside a class. Always uppercase first letter
+//Java filename must match the class name
+public class MyClass {
+	public static void main(String[] args) { //main() will be executed
+		System.out.println("Hello World");
+	}
+}
